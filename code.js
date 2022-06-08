efe21ab4a4c3b69b5f968bcaf6da01b9d4935517
Donovan Knight <99293948+dkthedeveloper@users.noreply.github.com>
2022-06-08 14:32:55 -0700

    Fixed bug that took user back to beginning when no page is selected. I also added a function that prompts user for a page number when none is selected

--- code.js
+++ code.js
@@ -3,10 +3,14 @@ let skipToPages = [3, 6, 10, 11, 15, 18];
 let currentPage = 0
 let content = `<p>${pages[0].slice(0, -22)}</p>`
 
-let convertType = function (value){
-    if (value === null) {
+function convertType (promptValue){
+    if (promptValue === undefined || promptValue === NaN || promptValue === '') {
+        promptValue = convertType(prompt(`Please enter a PAGE NUMBER\n
+        ${pages[currentPage]}`))
+    }
+    if (promptValue === null) {
         return value
-    } else return Number(value)
+    } else return Number(promptValue)
 }
 let isEnding = function ()  {
     return endingPages.includes(currentPage)
@@ -17,12 +21,15 @@ let isSkipToPage = function ()  {
 let finish = function () {
     document.write(content)
 }
+let gameInstructions = 'Welcome to "Choose Your Adventure"!\n To play this game, you must respond to\n prompts by entering the number of the page you\nwish to turn to. Have fun!';
 
+alert(gameInstructions);
 currentPage = convertType(prompt(`${pages[currentPage]}`))
 while (currentPage !== null) {
 
+
   if (!isEnding(currentPage)) {
- content += `<p>You turned to page ${currentPage}.</p><p>${pages[currentPage]}`;
+  content += `<p>You turned to page ${currentPage}.</p><p>${pages[currentPage]}`;
     if (!isSkipToPage(currentPage)) {
         currentPage = convertType(prompt(`${pages[currentPage]}\n Which page would you like to turn to?`))
     }   else {
@@ -30,10 +37,16 @@ while (currentPage !== null) {
     }
 } else {
     content += `<p>You turned to page ${currentPage}.</p><p>${pages[currentPage]}</p><h2>The End!</h2>`;
+    content += `<div>
+                <button onClick="window.location.href=window.location.href">Try Again?</button>
+                </div>`
     currentPage = null
 }
 } 
+
 finish()
+
+
 
 
 
